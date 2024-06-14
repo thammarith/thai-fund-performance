ec398537eea2b34b7f86a488530eb8a45c0170ff
Thammarith Likittheerameth <thammarith.likittheerameth@agoda.com>
2024-06-15 00:16:58 +0700

    Update paths

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,4 @@
-export const AMCS_PATH = './src/data/amcs/amcs.json'
-export const FUNDS_PATH = './src/data/funds/{amc}.json'
-export const NAV_PATH = './src/data/navs/{nav}.json'
-export const META_FETCH_LOG_PATH = './src/data/meta/fetch-log.json'
+export const AMCS_PATH = './data/amcs/amcs.json'
+export const FUNDS_PATH = './data/funds/{amc}.json'
+export const NAV_PATH = './data/navs/{nav}.json'
+export const META_FETCH_LOG_PATH = './data/meta/fetch-log.json'
